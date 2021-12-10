9385aafeba5506efee788ff39afb16e50c8c7902
TURBOCRINGE <75144909+TURBOCRINGE@users.noreply.github.com>
2021-12-10 16:24:27 -0500

    Update project.c

--- project.c
+++ project.c
@@ -503,23 +503,19 @@ void updateState(){
 /* Main */
 /******************************************************************************/
 
-int main()
-{
-  setbuf(stdout, NULL);
-    
-  // parse instructions into binary format
-  int counter = get_instructions(MEM_Instruction);
-  
-  // load program and run
-  copy_bits(ZERO, PC);
-  copy_bits(THIRTY_TWO, MEM_Register[29]);
-  
-  while (binary_to_integer(PC) < counter) {
-    print_instruction();
-    updateState();
-    print_state();
-  }
+int main(){
+	setbuf(stdout, NULL);
+	// parse instructions into binary format
+	int counter = get_instructions(MEM_Instruction);
+	// load program and run
+	copy_bits(ZERO, PC);
+	copy_bits(THIRTY_TWO, MEM_Register[29]);
 
-  return 0;
+	while (binary_to_integer(PC) < counter) {
+		print_instruction();
+		updateState();
+		print_state();
+	}
+	return 0;
 }
 
