d60b7fd02056ef7c13fd4e2ae00fde212d846204
Curtex02 <54672604+Curtex02@users.noreply.github.com>
2021-12-10 19:59:33 -0500

    Update project.c

--- project.c
+++ project.c
@@ -513,11 +513,13 @@ void ALU_Control(BIT* ALUOp, BIT* funct, BIT* ALUControl){  // curtis
 
 void ALU1(BIT A, BIT B, BIT Binvert, BIT CarryIn, BIT Less, 
   BIT Op0, BIT Op1, BIT* Result, BIT* CarryOut, BIT* Set){
+	//from lab 6
 	BIT x0 = multiplexor2(Binvert, B, not_gate(B));
 
 	BIT y0 = and_gate(A, x0);
 	BIT y1 = or_gate(A, x0);
 	BIT y2 = FALSE;
+
 	adder1(A, x0, CarryIn, CarryOut, &y2);  
 	*Result = multiplexor4(Op0, Op1, y0, y1, y2, Less);
 	*Set = y2;
